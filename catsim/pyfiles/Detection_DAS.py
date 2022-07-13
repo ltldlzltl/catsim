94bcfed67a37ad315e7f9e0758d03b87bf6ecba2
Tianling Lyu <lvfucius@gmail.com>
2022-07-13 09:28:36 +0800

    more realistic detector gain

--- catsim/pyfiles/Detection_DAS.py
+++ catsim/pyfiles/Detection_DAS.py
@@ -3,9 +3,12 @@
 import numpy as np
 
 def Detection_DAS(viewIn, cfg):
-    viewOut = viewIn*cfg.scanner.detectionGain
+    viewOut = viewIn * (1.6*1e-7/cfg.scanner.capacitor
+        /cfg.scanner.readoutVolt*cfg.scanner.maxReadout)
+    viewOut = viewOut*cfg.scanner.detectionGain
     
     eNoise = np.float32(np.random.randn(viewIn.size)*cfg.sim.eNoise)
     viewOut += eNoise.reshape(viewIn.shape)
+    viewOut[viewOut>cfg.scanner.maxReadout] = cfg.scanner.maxReadout
     
     return viewOut
